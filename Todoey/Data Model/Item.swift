9c7d821dce8fefb349db4ab58774ab70d6b28544
Abdulrahman jahdami <abdulrahmanaljahdami@abdulrahmans-macbook-pro.local>
2018-02-10 19:00:40 +0000

    convert to MVS digin

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Abdulrahman Al jahdami on 10/02/2018.
+//  Copyright © 2018 Abdulrahman Al jahdami. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done  : Bool = false
+}
